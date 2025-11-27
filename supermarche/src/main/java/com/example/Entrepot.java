c8454488de73d2e5ac03a98e18c498ddd5f31232
btuaux <baptiste.tuaux@etudiant.univ-rennes.fr>
2025-11-27 10:21:08 +0100

    refactor: update Rayon class to manage a single product and its maximum capacity

---
+++ supermarche/src/main/java/com/example/Entrepot.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.util.List;
+
+public class Entrepot{
+    private List<Produit> availableProducts;
+
+    public Entrepot(List<Produit> availableProducts) {
+        this.availableProducts = availableProducts;
+    }
+
+
+    public List<Produit> getAvailableProducts() {
+        return availableProducts;
+    }
+
+    public Produit getProductById(int id) {
+        for (Produit produit : availableProducts) {
+            if (produit.getId() == id) {
+                return produit;
+            }
+        }
+        return null; // or throw an exception if preferred
+    }
+
+}
--- supermarche/src/main/java/com/example/Rayon.java
+++ supermarche/src/main/java/com/example/Rayon.java
@@ -3,10 +3,31 @@ package com.example;
 import java.util.List;
 
 public class Rayon{
-    private List<Produit> produits;
+    private Produit produit; 
+    private int nb_product_max;
+    private int currentAmountproducts = nb_product_max;
     
-    public Rayon(List<Produit> produits) {
-        this.produits = produits;
+    public Rayon( Produit produit, int nb_product_max) {
+        this.produit = produit;
+        this.nb_product_max = nb_product_max;
     }
+
+    public Produit getProduit() {
+        return produit;
+    }
+
+    public int getNb_product_max() {
+        return nb_product_max;
+    }
+
+    public int getCurrentAmountproducts() {
+        return currentAmountproducts;
+    }
+
+    public void setCurrentAmountproducts(int currentAmountproducts) {
+        this.currentAmountproducts = currentAmountproducts;
+    }
+
+    
 
 }
